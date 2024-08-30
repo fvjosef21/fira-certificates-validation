d3af702e80510097fe87134dab74781c29e368fb
Josef Krein <fvjosef21@outlook.com>
2024-08-30 11:51:35 +0800

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-import App from './App.tsx';
-import './index.css';
-
-const router = createBrowserRouter([
-  {path:"fira-certificates-validation/:event", element: <App />,}
-]);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </StrictMode>,
 )
