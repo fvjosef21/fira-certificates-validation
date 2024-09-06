b03e9e3d71bbfaf41c4518c52e69306ebcefc8fa
Josef Krein <fvjosef21@outlook.com>
2024-09-06 23:00:35 +0800

    hex128

--- src/Certificate.tsx
+++ src/Certificate.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from 'react';
 import QRCode from "react-qr-code";
 import FIRA2024 from './assets/fira2024.svg';
 import {stringToBase64URL, stringFromBase64URL} from "./base64url";
-import {stringToArrayBuffer, concatenateArrayBuffers, arrayBufferToBase64, arrayBufferToHex} from './arraybuffer_utils';
+import {stringToArrayBuffer, arrayBufferToBase64, arrayBufferToHex} from './arraybuffer_utils';
 import './certificate.css';
 
 export interface Certificate {
